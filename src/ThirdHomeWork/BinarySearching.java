da3d71a55ea2d76d1bbbffd9405c4f5684ed7440
LaurentDEveck <vasilevartem91@yandex.ru>
2022-09-30 01:22:51 +0300

    New tasks have done

--- src/ThirdHomeWork/BinarySearching.java
+++ src/ThirdHomeWork/BinarySearching.java
@@ -4,14 +4,27 @@ import java.util.Scanner;
 
 public class BinarySearching {
     public static void main(String[] args) {
+        int max = 100;
+        int min = 2;
+        int compNum = (int) (Math.random() * ((max - min) + 1));
         Scanner in = new Scanner(System.in);
         System.out.println("Введите число от 2 до 100 ");
         int userNum = in.nextInt();
         if (userNum < 2 && userNum > 100) {
-            System.out.println("Ошибка");;
+            System.out.println("Ошибка");
+            ;
         } else {
-            System.out.println("Число больше 50 ?");
-
+            while (compNum != userNum) {
+                if (compNum > userNum) {
+                    compNum = compNum / 2;
+                } else if (compNum < userNum) {
+                    compNum += 1;
+                } else {
+                    break;
+                }
+                System.out.println("Загаданное число = " + compNum);
+            }
         }
     }
 }
+
--- src/ThirdHomeWork/GuessingNumber.java
+++ src/ThirdHomeWork/GuessingNumber.java
@@ -7,25 +7,24 @@ public class GuessingNumber {
         double compNum = (Math.random() * ((9 - 1) + 1)) + 1;
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Введите число от 1 до 9");
+        System.out.println("Введите число от 1 до 9. Для выхода нажмите 0");
         double userNum = input.nextDouble();
-        if (userNum < 1 && userNum > 9) {
-            System.out.println("Ошибка");
-        } else if (userNum == 0) {
+        if (userNum < 1 && userNum > 9) System.out.println("Ошибка");
+        if (userNum == 0) {
             System.out.println("Выход из программы");
             return;
         } else {
             while (userNum != compNum) {
+
                 if (userNum > compNum) {
                     System.out.println("Загаданное число меньше");
-                    return;
-                } else if (userNum < compNum){
+
+                }
+                if (userNum < compNum) {
                     System.out.println("Загаданное число больше");
-                    return;
-                } else {
-                    System.out.println("Вы угадали!");
+                    }
                 }
             }
         }
     }
-}
+
