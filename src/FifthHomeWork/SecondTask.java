03e9cdbe33e491b95755b6ea2cc9c8cac8b55ef8
LaurentDEveck <vasilevartem91@yandex.ru>
2022-10-02 22:26:18 +0300

    second task

--- src/FifthHomeWork/SecondTask.java
+++ src/FifthHomeWork/SecondTask.java
@@ -5,23 +5,19 @@ import java.util.Arrays;
 public class SecondTask {
     public static void main(String[] args) {
         int[] arr1 = new int[10];
-        for (int i = 0, j = 2; i < arr1.length; i++, j += 2) {
-            arr1[i] = j;
 
-          /*  for (int i = 0; i < arr1.length / 2; i+=2) {
-                int temp = numbers[i];
-                numbers[i] = numbers[numbers.length - 1 - i];
-                numbers[numbers.length - 1 - i] = temp; */
 
-            System.out.print(Arrays.toString(arr1));
-         /*    int[] arr2 = new int[arr1.length];
+            int[] arr2 = new int[arr1.length];
 
-            for (int m = 2; i < arr1.length; m++) {
+            for (int m = 2; m < arr1.length; m++) {
                 arr2[arr1.length - 1 - m] = arr1[m];
                 System.out.println(arr1[m] + " ");
 
-            } */
+                System.out.print(Arrays.toString(arr1));
+
+
+            }
         }
     }
-}
+
 
--- src/FifthHomeWork/Task4.java
+++ src/FifthHomeWork/Task4.java
@@ -3,7 +3,8 @@ package FifthHomeWork;
 public class Task4 {
     public static void main(String[] args) {
         String string1 = "в предложении все слова разной длины";
-        String[] strings = new String[]{string1};
+        String[] arrStr = string1.split(";");
+        System.out.println(arrStr);
 
     }
 }
