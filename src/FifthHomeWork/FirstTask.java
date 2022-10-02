dec480c35779e1056493176e80aa77a45594b40f
LaurentDEveck <vasilevartem91@yandex.ru>
2022-10-02 21:58:58 +0300

    First task has done

--- src/FifthHomeWork/FirstTask.java
+++ src/FifthHomeWork/FirstTask.java
@@ -11,13 +11,39 @@ public class FirstTask {
         System.out.println("Введите длинну массива");
         int a = input.nextInt();
         int[] arr1 = new int[a];
-        System.out.println(Arrays.toString(arr1));
 
         for (int i=0; i<arr1.length; i++)
             arr1[i] = (int) (Math.random() * a);
         for (int i: arr1)
             System.out.print( i + ", " );
 
+        int maxNum = arr1[0];
+
+        for (int j : arr1) {
+            if (j > maxNum)
+                maxNum = j;
+        }
+
+        System.out.println("Максимальный элемент = " + maxNum);
+
+        int minNum = arr1[0];
+
+        for (int j : arr1) {
+            if (j < minNum)
+                minNum = j;
+        }
+        System.out.println("Минимальный элемент = " + minNum);
+
+        int averageArr = 0;
+
+        int sumArr = 0;
+        for (int j = 0; j < arr1.length; j++) {
+            sumArr += arr1[j];
+        }
+        averageArr = sumArr / arr1.length;
+
+        System.out.println("Среднее арифметическое элементов = " + averageArr);
 
     }
 }
+
